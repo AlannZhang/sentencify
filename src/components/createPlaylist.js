e575dcbb0a7c6be7fe1b18a4da27adcd8320bcbe
Alan Zhang <alan.zhang.jedi@gmail.com>
2021-04-27 14:17:08 -0400

    implementing adding songs to the playlist

--- src/components/createPlaylist.js
+++ src/components/createPlaylist.js
@@ -1,14 +1,23 @@
 import { React, useState, useEffect } from 'react';
 import axios from 'axios';
-import { Button, Form, Row } from 'react-bootstrap';
+import { Button, Form, Row, Col, ListGroup } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 require('dotenv').config();
 
 const CreatePlaylist = () => {
+  const history = useHistory();
   const token = JSON.parse(localStorage.getItem('token'));
   const [userId, setUserId] = useState('');
   const [formData, setFormData] = useState('');
+  const [songUri, setSongUri] = useState([])
+  const [songNames, setSongNames] = useState([]);
+  const [artistNames, setArtistNames] = useState([]);
+  const [playlistId, setPlaylistId] = useState('');
+  const [showForm, setShowForm] = useState(true);
+  const randomSong = Math.floor(Math.random() * 20);
   let formArr = [];
 
+  // get user id on render
   useEffect(() => {
     const getUserInfo = async () => {
       try {
@@ -21,7 +30,7 @@ const CreatePlaylist = () => {
         }
 
         const results = await axios(reqParams);
-        setUserId(results.data.id)
+        setUserId(results.data)
       } catch (err) {
         console.error(err);
       }
@@ -30,6 +39,7 @@ const CreatePlaylist = () => {
     getUserInfo();
   }, []);
     
+  // create the playlist with name set as form input
   const createPlaylist = async () => {
     try {
       const reqParams = {
@@ -46,63 +56,115 @@ const CreatePlaylist = () => {
       }
 
       const results = await axios(reqParams);
-      console.log(results);
+      console.log(results.data.id);
+      setPlaylistId(results.data.id);
     } catch (err) {
       console.error(err);
     }
   };
 
-  const getSongs = async () => {
+  // add songs to the playlist
+  const addSongs = async () => {
     try {
-      for (let i = 0; i < formArr.length; i++) {
+      for (let i = 0; i < songUri.length; i++) {
         const reqParams = {
-          method: 'get',
-          url: 'https://api.spotify.com/v1/search',
+          method: 'post', 
+          url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks?position=${i}&uris=${songUri}`,
           headers: {
             'Authorization': `Bearer ${token}`,
-          },
-          data: {
-            q: `${formArr[i]}`,
-            type: 'track',
-            limit: '10',
           }
-        }
+        };
 
-        console.log(formArr[i]);
         const results = await axios(reqParams);
         console.log(results);
       }
     } catch (error) {
       console.error(error);
     }
-  }
+  };
+
+  // search for songs based on each word from the form input
+  const getSongs = async () => {
+    try {
+      for (let i = 0; i < formArr.length; i++) {
+        const reqParams = {
+          method: 'get',
+          url: `https://api.spotify.com/v1/search?q=${formArr[i]}&type=track&limit=20`,
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        };
+
+        const results = await axios(reqParams);
+        setSongUri((arr) => [...arr, results.data.tracks.items[randomSong].uri]);
+        setSongNames((arr) => [...arr, results.data.tracks.items[randomSong].name]);
+        console.log(`Song: ${results.data.tracks.items[randomSong].name}`);
+
+        for (let j = 0; j < results.data.tracks.items[randomSong].artists.length; j++) {
+          console.log(`Artist: ${results.data.tracks.items[randomSong].artists[j].name}`);
+          setArtistNames((arr) => [...arr, results.data.tracks.items[randomSong].artists[j].name]);
+        }
+      }
+
+      addSongs();
+    } catch (error) {
+      console.error(error);
+    }
+  };
     
   const onSubmit = (e) => {
     e.preventDefault();
     formArr = formData.split(' ');
-    // createPlaylist();
+    createPlaylist();
     getSongs();
+    setShowForm(false);
   };
+
+  const logOut = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    history.push('/');
+  }
 
   return (
     <>
       <Row className='justify-content-center'>
-        <h1 style={{textAlign: 'center', margin: '30px auto auto'}}> Setencify </h1>
+        <Col md={12}>
+          <h1 style={{textAlign: 'center', margin: '30px auto auto'}}> Setencify </h1>
+        </Col>
+        <Col>
+          <Button
+            variant='link'
+            onClick={logOut}
+          >
+            Logout
+          </Button>
+        </Col>
       </Row>
       <Row className='justify-content-center' style={{margin: '100px auto auto'}}>
-      <Form name='testform' onSubmit={onSubmit}>
-          <input 
-            type='text' 
-            required
-            className='form' 
-            value={formData}
-            onChange={(e) => setFormData(e.target.value)}
-            placeholder='Type a sentence...'
+        {showForm && (
+          <Form name='testform' onSubmit={onSubmit}>
+            <input 
+              type='text' 
+              required
+              className='form' 
+              value={formData}
+              onChange={(e) => setFormData(e.target.value)}
+              placeholder='Type a sentence...'
           />
-        </Form>
+          </Form>
+        )}
+        <ListGroup>
+          {songNames.map((item) => (
+            <ListGroup.Item>Song: {item}</ListGroup.Item>
+          ))}
+          {artistNames.map((item) => (
+            <ListGroup.Item>Artist: {item}</ListGroup.Item>
+          ))}
+        </ListGroup>
       </Row>
     </>
-  )
+  );
 
 };
 
--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,5 @@
 import { React } from 'react';
-import { Button, Container, Row } from 'react-bootstrap';
+import { Button, Row } from 'react-bootstrap';
 import { authEndpoint, redirectUri, scopes } from './config';
 require('dotenv').config();
 
