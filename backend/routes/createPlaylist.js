fc22da801a75ba62fea738b7e143ba8af767574c
Alan Zhang <alan.zhang.jedi@gmail.com>
2021-04-27 17:51:13 -0400

    code cleanup

--- backend/routes/createPlaylist.js
+++ backend/routes/createPlaylist.js
@@ -3,25 +3,25 @@ const axios = require('axios');
 
 router.post('/:userId', (async (req, res) => {
   try {
-      const reqParams = {
-        method: 'post',
-        url: `https://api.spotify.com/v1/users/${req.params.userId}/playlists`,
-        headers: {
-          'Authorization': `Bearer ${req.body.token}`,
-        },
-        data: {
-          'name': `${req.body.formData}`,
-          'description': 'Playlist generated from the sentencify app',
-          'public': true,
-        }
-      };
+    const reqParams = {
+      method: 'post',
+      url: `https://api.spotify.com/v1/users/${req.params.userId}/playlists`,
+      headers: {
+        'Authorization': `Bearer ${req.body.token}`,
+      },
+      data: {
+        'name': `${req.body.formData}`,
+        'description': 'Playlist generated from the sentencify app',
+        'public': true,
+      }
+    };
 
-      const results = await axios(reqParams);
-      console.log(results.data);
-      res.send(results.data.id);
-    } catch (err) {
-      console.error(err);
-    }
+    const results = await axios(reqParams);
+    console.log(results.data);
+    res.send(results.data.id);
+  } catch (err) {
+    console.error(err);
+  }
 }));
 
 module.exports = router;
