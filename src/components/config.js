dfc48bf5d33fc2ecb2b389dfdb8cfffafa2b831d
Alan Zhang <alan.zhang.jedi@gmail.com>
2021-12-12 00:46:23 -0500

    testing redirect uri using netlify

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
-export const redirectUri = 'http:///sentencify.netlify.com/.netlify/functions/callback';
+export const redirectUri = 'http://sentencify.netlify.com/.netlify/functions/callback';
 export const scopes = [
   'playlist-modify',
   'playlist-modify-private',
